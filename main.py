7cf8d8eba592a4e293ef8f3416b33db3528b832f
Matt Brown <18365006+brown-m@users.noreply.github.com>
2021-10-29 14:14:19 +0000

    Updated command

--- main.py
+++ main.py
@@ -4,27 +4,19 @@ import os
 
 def main():
     try:
-        connection = psycopg2.connect(user=os.environ["DB_USERNAME"],
-                                    password=os.environ["DB_PASSWORD"],
-                                    host=os.environ["DB_HOST"],
-                                    port=os.environ["DB_PORT"],
-                                    database=os.environ["DB_DATABASE"])
-        cursor = connection.cursor()
-
-        # Refresh the view
-        print("Refreshing materialized view " + os.environ["INPUT_VIEW"])
-        cursor.execute(f'refresh materialized view {os.environ["INPUT_VIEW"]};')
-        print("DONE")
-
+        with psycopg2.connect(user=os.environ["DB_USERNAME"], 
+            password=os.environ["DB_PASSWORD"], 
+            host=os.environ["DB_HOST"], 
+            port=os.environ["DB_PORT"], 
+            database=os.environ["DB_DATABASE"]) as connection:
+            with connection.cursor() as cursor:
+                connection.autocommit = True
+                print("Refreshing materialized view " + os.environ["INPUT_VIEW"])
+                cursor.execute(f'refresh materialized view {os.environ["INPUT_VIEW"]};')
+                exit()
 
     except (Exception, psycopg2.Error) as error:
         print("Failed to refresh materialized view: ", error)
-
-    finally:
-        # Close connection.
-        if connection:
-            cursor.close()
-            connection.close()
 
 if __name__ == "__main__":
     main()
