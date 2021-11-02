24e91cd43a63107fd62ea6829eb534636b0ff13f
Matt Brown <18365006+brown-m@users.noreply.github.com>
2021-11-02 11:14:18 +0000

    Added retries to the query

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,11 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const RETRIES = 3
+
 func main() {
+	failures := 0
+
 	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatal("Unable to create database connection: ", err)
@@ -19,9 +23,15 @@ func main() {
 	fmt.Printf("Refreshing materialized view %s...\n", os.Getenv("INPUT_VIEW"))
 	query := "refresh materialized view " + os.Getenv("INPUT_VIEW")
 
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal("Failed to execute query: ", err)
+	for failures < RETRIES {
+		_, err = db.Exec(query)
+		if err != nil {
+			fmt.Printf("Failed to execute query: %s\n", err)
+			failures += 1
+			fmt.Println("Retrying...")
+		} else {
+			break
+		}
 	}
 
 	fmt.Println("...DONE")
